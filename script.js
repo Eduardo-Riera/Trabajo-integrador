953f12239e08596f01c010dd1674bcf98d150b63
Eduardo <juanpe752@gmail.com>
2023-09-18 06:14:37 -0300

    definitivo

--- script.js
+++ script.js
@@ -2,15 +2,13 @@ var botonColor = document.getElementById("botonColor");
 botonColor.addEventListener("click", color);
 
 function color(){
-    const collection = ['green', 'gray', 'pink', 'yellow', 'red', 'blue'];
+    const collection = ['green', 'gray', 'pink', 'yellow', 'red', 'blue', 'orange', 'brown', 'white', 'black'];
     let i = 0;
         setInterval (function(){
-            if (i<6){
+            if (i<10){
                 document.body.style.backgroundColor = collection[i];
                 i++
-            }else{
-                i=0;
             }
-        }, 1000);        
+        }, 500);        
     };
 
